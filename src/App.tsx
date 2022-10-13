3e1ea9f14222f91bdafc65787c6432ada815f8cd
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2022-10-13 08:15:53 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import styles from './App.module.scss'
+import style from './Global/style.module.scss'
+import { Home } from './Home/Home'
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import { Dashboard } from './assets/Pages/Dashboar/Dash'
+function App() {
+  return (
+    <div className={styles.App}>
+      <div className={style.container}> 
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+          </Routes>
+        </Router> 
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/Pages/Dashboar/Dash.tsx
@@ -0,0 +1,10 @@
+import style from './dash.module.scss'
+export function Dashboard(){
+    return(
+        <div className={style.container}>
+            <main>
+
+            </main>
+        </div>
+    )
+}
